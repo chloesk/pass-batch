0f92f39fe0e5e93a8cd3b1260273b1ead0aec11d
chloe k <chloe.ksh.kim@gmail.com>
2023-12-09 19:48:58 -0500

    Add basic Spring Batch Job and Step setup in Spring Boot
    Refactored batch job and step configuration for Spring Batch 5.1.0 compatibility. Replaced deprecated JobBuilderFactory and StepBuilderFactory with direct usage of JobBuilder and StepBuilder. Updated tasklet step to include transaction manager as per new Spring Batch guidelines.

--- src/main/java/com/fitness/pass/PassBatchApplication.java
+++ src/main/java/com/fitness/pass/PassBatchApplication.java
@@ -1,11 +1,38 @@
 package com.fitness.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
+
+@EnableBatchProcessing
 @SpringBootApplication
 public class PassBatchApplication {
 
+	@Bean
+	public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager){
+		return new StepBuilder("passStep", jobRepository)
+				.tasklet((contribution, chunkContext) -> {
+					System.out.println("Execute PassStep");
+					return RepeatStatus.FINISHED;
+				}, transactionManager)
+				.build();
+	}
+
+	@Bean
+	public Job passJob(JobRepository jobRepository, Step passStep) {
+		return new JobBuilder("passJob", jobRepository)
+				.start(passStep)
+				.build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
 	}
